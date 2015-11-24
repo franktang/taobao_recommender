6b337ef60a83c31ed0a40971b014526d5983185b
Gao, Chao <supergrubby@gmail.com>
2015-11-24 20:59:50 +0800

    update filter and result
    only output item in the test_items

--- cf/filter_traing_results.py
+++ cf/filter_traing_results.py
@@ -2,6 +2,8 @@ import csv
 
 item_cat = {}
 
+arr_test_items = []
+
 
 def is_item_same_category(item1_id, item2_id):
     item1_cat = item_cat[item1_id]
@@ -12,12 +14,24 @@ def is_item_same_category(item1_id, item2_id):
         return False
 
 
+def is_in_test_items(item):
+    if item in arr_test_items:
+        return True
+    else:
+        return False
+
+
 with open("dim_items.txt") as items, open("similar_itmes_traing_result.csv") as traing_result, open("filter_result.csv",
-                                                                                      "wb") as filter_result:
+                                                                                                    "wb") as filter_result, open(
+    "test_items.txt") as test_items:
     for line in items:
         item_info = line.strip().split(" ")
         item_cat[item_info[0]] = item_info[1]
         # print(item_cat[item_info[0]])
+
+    for line in test_items:
+        test_item = line.strip()
+        arr_test_items.append(test_item)
 
     csv_writer = csv.writer(filter_result)
     title = ["item_id", "similar", "score", "rank"]
@@ -27,5 +41,6 @@ with open("dim_items.txt") as items, open("similar_itmes_traing_result.csv") as 
     for line in traing_result:
         sim_info = line.strip().split(",")
         # print(sim_info)
-        if is_item_same_category(sim_info[0], sim_info[1]):
+        if is_item_same_category(sim_info[0], sim_info[1]) and (
+            is_in_test_items(sim_info[0]) or is_in_test_items(sim_info[1])):
             csv_writer.writerow([x for x in sim_info])
